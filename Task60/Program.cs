18e06382437261026a8a8f5130d58cb806dd1fb9
Anastasiya Z <nastya_tim2201@mail.ru>
2022-11-22 00:40:37 +0300

    решила четвертую задачу

---
+++ Task60/Program.cs
@@ -0,0 +1,42 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.Clear();
+
+int[,,] FillMatrix3DRndInt(int rows, int columns, int depth)
+{
+int[,,] matrix = new int[rows, columns, depth];
+
+Random rnd = new Random();
+int value = rnd.Next(10,20);
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(2); k++)
+        {
+            matrix[i, j, k] = value+(i+j+k);
+            value += 5;
+        }
+    }
+}
+return matrix;
+}
+
+void PrintMatrix3D(int[,,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(2); k++)
+        {
+            Console.Write($"{matrix[i,j,k]} ({i}, {j}, {k}) ");
+        }
+    }
+    Console.WriteLine();
+}
+}
+
+int[,,] massive3D = FillMatrix3DRndInt(2,2,2);
+PrintMatrix3D(massive3D);
